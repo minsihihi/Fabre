c00be21580e7bbd14e7023cdcbf3f6b09b2a8823
Jin0328 <chloe02.han@gmail.com>
2025-01-23 17:27:08 +0900

    데이터베이스 user id 추가 수정

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt'); // 비밀번호 해싱
+const jwt = require('jsonwebtoken'); // JWT 토큰 생성
+const { User } = require('../models');
+
+
+router.post('/register', async(req, res) => {
+    try {
+        const { username, password, role } = req.body; // 클라이언트로부터 
+
+        const 
+    }
+})
 
 router.get('/', (req, res) => {
     res.send('Test');
