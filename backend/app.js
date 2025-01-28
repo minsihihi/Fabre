c5916df87cffc269cd2ec74c80e641219e4c178c
Jin0328 <chloe02.han@gmail.com>
2025-01-28 17:04:59 +0900

    회원별 로그인 화면 수정

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-const express = require('express');
 const dotenv = require('dotenv');
+dotenv.config({ path: 'backend/.env' });
+const express = require('express');
 const db = require('./models');
 const apiRoutes = require('./routes/api'); 
-
-dotenv.config({ path: '../.env' });
 
 const app = express();
 app.use(express.json());
 app.use('/api', apiRoutes);
+
 
 db.sequelize
     .authenticate()
