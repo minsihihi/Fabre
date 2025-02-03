eedb9ad55a9895289634f8b3f3289839a47c2a53
Jin0328 <chloe02.han@gmail.com>
2025-02-03 21:21:11 +0900

    로그아웃 백엔드 수정

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,18 @@ dotenv.config({ path: 'backend/.env' });
 const express = require('express');
 const db = require('./models');
 const apiRoutes = require('./routes/api'); 
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 app.use(express.json());
 app.use('/api', apiRoutes);
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+app.use(cookieParser());
 
 
 db.sequelize
