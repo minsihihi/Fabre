596bcbbcdea0f6de53fe976d5775928745cded33
Jin0328 <chloe02.han@gmail.com>
2025-02-08 23:17:30 +0900

    트레이너 회원 설정

--- frontend/main.js
+++ frontend/main.js
@@ -12,7 +12,7 @@ function createWindow() {
 
     win.loadFile('frontend/src/index.html');
 
-    // win.webContents.openDevTools();
+    win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
