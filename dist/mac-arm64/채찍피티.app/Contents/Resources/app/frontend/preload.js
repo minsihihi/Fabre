e67aadf73f73f0ec065d68a55d7e9aa017b7fcb4
Jiin <chloe02.han@gmail.com>
2025-04-11 02:42:58 +0900

    [Deploy]

--- dist/mac-arm64/채찍피티.app/Contents/Resources/app/frontend/preload.js
+++
@@ -1,6 +0,0 @@
-// frontend/preload.js
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  // 예시: ipcRenderer.on('channel', callback) 등 작성 가능
-});
