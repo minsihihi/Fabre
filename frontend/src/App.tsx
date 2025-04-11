1a314e4c098cdecc0ecc744e14c8c976c0285e37
Jiin <chloe02.han@gmail.com>
2025-04-11 17:47:23 +0900

    [Update] 로그인 페이지 안 뜨는 문제 해결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import Login from './Login';
 import Register from './Register';
