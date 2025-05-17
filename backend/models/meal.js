7990523627f2697a1e14a78d62f2a3f2df4c4976
minsihihi <126065697+minsihihi@users.noreply.github.com>
2025-05-17 22:20:48 +0900

    Update meal.js

--- backend/models/meal.js
+++ backend/models/meal.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
             autoIncrement: true,
             primaryKey: true
         },
-        userId: {
+        user_Id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             references: {
