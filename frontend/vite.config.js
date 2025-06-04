227e52852b4487094bf44eaef14a993d4b35cad8
minsihihi <126065697+minsihihi@users.noreply.github.com>
2025-06-04 16:38:48 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,4 +12,13 @@ export default defineConfig({
       input: "./index.html", // ✅ 추가: 빌드 진입점을 명확히
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://13.209.19.146:3000/', // 백엔드 서버 주소로 변경
+        changeOrigin: true, // CORS 문제 해결
+        secure: false, // HTTPS에서 HTTP로의 요청 허용
+      },
+    },
+  },
 });
